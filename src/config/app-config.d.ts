71692e23d3540fc2d27d051543cde99e02981a89
diamond120 <diamond.at312@gmail.com>
2023-09-18 09:20:30 -0500

    Delete src/config/app-config.d.ts

--- src/config/app-config.d.ts
+++
@@ -1,8 +0,0 @@
-// src/config/app-config.d.ts
-
-// You can define the types for your app-config module here.
-// For example:
-declare module "../config/app-config" {
-  export const BASE_URL: string;
-  // Add other exports as needed.
-}
