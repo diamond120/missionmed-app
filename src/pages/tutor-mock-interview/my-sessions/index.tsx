d644f1118eb10f18fb0d341f2faab89f5643cfa4
diamond120 <diamond.at312@gmail.com>
2023-09-20 18:39:45 +0530

    tutor mock interview design

---
+++ src/pages/tutor-mock-interview/my-sessions/index.tsx
@@ -0,0 +1,137 @@
+import { useNavigate } from "react-router-dom";
+import "./index.less"
+import { Breadcrumb,  Button, Tabs} from "antd"
+
+const Mysessions = () => {
+     const { TabPane } = Tabs;
+     const navigation = useNavigate();
+     return (
+     <>
+     <div className={"upc-agenda con-box"} style={{marginTop:'55px'}}>
+          <h2 className={"secondary-title"}>My Sessions </h2>
+          <Tabs defaultActiveKey={"profile"}>
+          
+               <TabPane tab={"Upcoming"} key={"Upcoming"}>
+                    <div className={"upcoming-sessions"}>
+                         
+                         <div className="sessions">
+                              <h4 className="sessions-date">Fri, 16 Jun 2023</h4>
+                              <ul className="sessions-list"> 
+                                   <li className="item">
+                                        <div className="time">
+                                             <div style={{paddingBottom:"5px"}}><strong>3:00 pm</strong></div>
+                                             <div className={"end-time"}>3:00 pm</div>
+                                        </div>
+
+                                        <div>
+                                             <div style={{paddingBottom:"5px"}}><strong>UCAT Teaching Session</strong></div>
+                                             <div className={"mock_interview"}>Cameron Williamson</div>
+                                        </div>
+                                        <Button className={"secondary-button"}>Reschedule</Button>
+                                   </li>
+                                   
+                                   <li className="item">
+                                        <div className="time">
+                                             <div style={{paddingBottom:"5px"}}><strong>3:00 pm</strong></div>
+                                             <div className={"end-time"}>3:00 pm</div>
+                                        </div>
+
+                                        <div>
+                                             <div style={{paddingBottom:"5px"}}><strong>UCAT Teaching Session</strong></div>
+                                             <div className={"mock_interview"}>Cameron Williamson</div>
+                                        </div>
+                                        <Button className={"secondary-button"}>Reschedule</Button>
+                                   </li>
+                              </ul>
+                         </div>
+
+                         <div className="sessions">
+                              <h4 className="sessions-date">Fri, 16 Jun 2023</h4>
+                              <ul className="sessions-list"> 
+                                   <li className="item">
+                                        <div style={{display:"flex"}}>
+                                             <div className="time">
+                                                  <div style={{paddingBottom:"5px"}}><strong>3:00 pm</strong></div>
+                                                  <div className={"end-time"}>3:00 pm</div>
+                                             </div>
+
+                                             <div>
+                                                  <div style={{paddingBottom:"5px"}}><strong>UCAT Teaching Session</strong></div>
+                                                  <div className={"mock_interview"}>Cameron Williamson</div>
+                                             </div>
+                                        </div>
+                                        <Button className={"secondary-button"}>Reschedule</Button>
+                                   </li>
+                                   
+                              </ul>
+                         </div>
+                    </div>
+               </TabPane>
+
+               <TabPane tab={"Past"} key={"Past"}>
+                    <div className={"upcoming-past"}>
+                         <div className="sessions">
+                              <h4 className="sessions-date">Thus, 16 Jun 2023</h4>
+                              <ul className="sessions-list"> 
+                                 
+                                   <li className="item">
+                                        <div style={{display:"flex"}}>
+                                             <div className="time">
+                                                  <div style={{paddingBottom:"5px"}}><strong>3:00 pm</strong></div>
+                                                  <div className={"end-time"}>3:00 pm</div>
+                                             </div>
+
+                                             <div>
+                                                  <div style={{paddingBottom:"5px"}}><strong>UCAT Teaching Session</strong></div>
+                                                  <div className={"mock_interview"}>Cameron Williamson</div>
+                                             </div>
+                                        </div>
+                                        <Button className={"secondary-button"} onClick={navigation('/tutor/session-summary')}>Session Summary</Button>
+                                   </li>
+                              </ul>
+                         </div>
+                         <div className="sessions">
+                              <h4 className="sessions-date">Thus, 16 Jun 2023</h4>
+                              <ul className="sessions-list"> 
+                                   <li className="item">
+                                        <div style={{display:"flex"}}>
+                                             <div className="time">
+                                                  <div style={{paddingBottom:"5px"}}><strong>3:00 pm</strong></div>
+                                                  <div className={"end-time"}>3:00 pm</div>
+                                             </div>
+
+                                             <div>
+                                                  <div style={{paddingBottom:"5px"}}><strong>UCAT Teaching Session</strong></div>
+                                                  <div className={"mock_interview"}>Cameron Williamson</div>
+                                             </div>
+                                        </div>
+                                        <Button className={"secondary-button"}>Session Summary</Button>
+                                   </li>
+                                   <li className="item">
+                                        <div style={{display:"flex"}}>
+                                             <div className="time">
+                                                  <div style={{paddingBottom:"5px"}}><strong>3:00 pm</strong></div>
+                                                  <div className={"end-time"}>3:00 pm</div>
+                                             </div>
+
+                                             <div>
+                                                  <div style={{paddingBottom:"5px"}}><strong>UCAT Teaching Session</strong></div>
+                                                  <div className={"mock_interview"}>Cameron Williamson</div>
+                                             </div>
+                                        </div>
+                                        <Button className={"secondary-button"}>Session Summary</Button>
+                                   </li>
+
+                                   
+                                   
+                              </ul>
+                         </div>
+                    </div>
+               </TabPane>
+          </Tabs>
+     </div>
+     </>
+     )
+}
+
+export default Mysessions
