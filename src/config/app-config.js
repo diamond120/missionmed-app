b0b7f3e3c96e5ff3f858d118edc7c25a2e8d6c39
diamond120 <diamond.at312@gmail.com>
2024-03-15 03:05:16 -0500

    Update app-config.js

--- src/config/app-config.js
+++ src/config/app-config.js
@@ -1,4 +1,4 @@
 export const API_URL = import.meta.env.VITE_API_URL
 export const BASE_URL  = `${API_URL}/api`
-export const GOOGLE_MAP_API_KEY = 'AIzaSyBoxgZYVseUclVAVW4uClSdkGbu_7YbJF4'
+export const GOOGLE_MAP_API_KEY = import.meta.env.VITE_GOOGLE_MAP_API_KEY
 export const TINYMCE_API_KEY =  import.meta.env.VITE_TINYMCE_API_KEY
