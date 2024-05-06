fb5493755e9c686f4a95147441c692d38b72582b
diamond120 <diamond.at312@gmail.com>
2024-05-06 13:05:54 +0530

    without login calendar changes

--- src/components/layout/calendarLayout.tsx
+++ src/components/layout/calendarLayout.tsx
@@ -1,33 +1,70 @@
 import "./calendarLayout.less"
-import { FC } from "react"
+import { FC, useState, useMemo, useEffect} from "react"
 import { Tabs } from "antd"
 import TutorCalendarTeaching from "../../pages/tutor-calendar-teaching"
 import TutorCalendarMock from "../../pages/tutor-calendar-mock"
+import { useParams } from 'react-router-dom';
+import CommonService from "../../api/services/Common";
 export const calendarLayout: FC = () => {
-  const { TabPane } = Tabs;   
+  const { TabPane } = Tabs;
+  const [slots, setSlots] = useState([]);
+  const [spin, setSpin] = useState(false);
+  const { ID } = useParams();
+  const calTutorId = parseInt(ID); 
+  useEffect(() => {
+    const fetchData = async () => {
+      setSpin(true);
+      try {
+   
+        const data = {
+          tutorId: calTutorId,
+        };
+
+        const response = await CommonService.postAPI("/tutors-session-check", data);
+        console.log(response)
+        if (response.data.success) {
+          const slotList = response.data.data ?? [];
+          setSlots(slotList);
+          console.log('cheking',slotList)
+        } else {
+          throw new Error(response.data.message);
+        }
+      } catch (error) {
+        message.error(error.message);
+      } finally {
+        setSpin(false);
+      }
+    };
+
+    fetchData();
+  }, [calTutorId]);
 return(
 <div className={"book-time-cal-wrap"}>
   <Tabs >
+  {slots.ucat_tutoring === 'true' && (
     <TabPane tab={"UCAT Teaching Session"} key={"ucat"}>
     <div className={"working-cal-wrap"}>
       <TutorCalendarTeaching />
       </div>
     </TabPane>
+     )}
+     {slots.interview_tutoring === 'true' && (
     <TabPane tab={"Interview Teaching Session"} key={"teaching"}>
     <div className={"working-cal-wrap"}>
       <TutorCalendarTeaching />
     </div>
     </TabPane>
+      )}
+    {slots.mock_interview === 'true' && (
     <TabPane tab={"Mock Interview"} key={"mock"}>
     <div className={"working-cal-wrap"}>
       <TutorCalendarMock />
     </div>
     </TabPane>
+   )}
   </Tabs>
 </div>
-
 )
-
 };
 
 export default calendarLayout;
