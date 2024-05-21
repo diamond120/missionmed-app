4604bc1e32c5c2ad80fe9e9ee437430bdd7f0523
diamond120 <diamond.at312@gmail.com>
2024-05-21 15:06:29 +0530

    front side api packages added auth

--- src/api/services/MockSimulation.tsx
+++ src/api/services/MockSimulation.tsx
@@ -8,7 +8,7 @@ export const getPackages = () => {
             'Authorization': token
         }
     }
-    return http.get(`/packages`, config);
+    return http.get(`/auth/packages`, config);
 }
 
 export const getSessions = () => {
