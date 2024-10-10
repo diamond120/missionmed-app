ade28becae67b89a488a678d50c3fd86652e2d49
diamond120 <diamond.at312@gmail.com>
2024-10-10 17:42:09 +0530

    always append bearer token and not found set as null

--- src/api/http-common.ts
+++ src/api/http-common.ts
@@ -1,37 +1,35 @@
-import axios from "axios";
-import { BASE_URL } from "../config/app-config";
-import { getToken } from '~/common/common';
+import axios from 'axios'
+import { BASE_URL } from '~/config/app-config'
+import { getToken } from '~/common/common'
 
 const http = axios.create({
-    //baseURL: process.env.API_URL,
-    baseURL: BASE_URL,
-    headers: {
-        "Content-type": "application/json",
-    },
-});
+  //baseURL: process.env.API_URL,
+  baseURL: BASE_URL,
+  headers: {
+    'Content-type': 'application/json'
+  }
+})
 http.interceptors.request.use((config) => {
-    const token = getToken();
+  const token = getToken() ?? null
 
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
+  config.headers.Authorization = `Bearer ${token}`
 
-    return config;
-});
+  return config
+})
 http.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    (error) => {
-        if (error.code === "ERR_NETWORK") {
-            // TODO: handle internal server error
-        } else if (error.response?.status === 401) {
-            // handle unauthorized request
-            localStorage.clear();
-            window.location.href = "/sign_in";
-        }
-        return Promise.reject(error);
+  (response) => {
+    return response
+  },
+  (error) => {
+    if (error.code === 'ERR_NETWORK') {
+      // TODO: handle internal server error
+    } else if (error.response?.status === 401) {
+      // handle unauthorized request
+      localStorage.clear()
+      window.location.href = '/sign_in'
     }
-);
+    return Promise.reject(error)
+  }
+)
 
-export default http;
+export default http
