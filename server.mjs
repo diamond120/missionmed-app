ece340371e045226d717a11a1609111c8a18fbd0
diamond120 <diamond.at312@gmail.com>
2024-11-22 13:52:32 +0530

    gopy google auth file to dist implemented

--- server.mjs
+++ server.mjs
@@ -10,6 +10,7 @@
 
 // app.listen(8080)
 
+import fs from 'fs'
 import express from 'express'
 import path from 'path'
 import os from 'os' // To get the network IP address
@@ -19,6 +20,21 @@ const app = express()
 import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url) // get the resolved path to the file
 const __dirname = path.dirname(__filename) // get the name of the directory
+
+try {
+  const sourcePath = path.join(__dirname, 'google5f2b2bf91c3f04a8.html')
+  const destPath = path.join(__dirname, 'dist', 'google5f2b2bf91c3f04a8.html')
+
+  fs.copyFile(sourcePath, destPath, (err) => {
+    if (err) {
+      console.error('Error copying file:', err)
+    } else {
+      console.log('custom.html copied successfully.')
+    }
+  })
+} catch (e) {
+  console.error(e.message)
+}
 
 // Serve static files from the "dist" directory
 app.use(express.static(path.join(__dirname, 'dist')))
@@ -51,7 +67,7 @@ app.listen(port, () => {
   const networkUrl = `http://${getNetworkAddress()}:${port}/`
 
   // Output similar to Vite's dev server output
-  console.log(chalk.green.bold(`\n  Express server ready\n`))
+  console.log(chalk.green.bold(`\n  Production server ready.\n`))
   console.log(`  ➜  Local:   ${chalk.cyan(localUrl)}`)
   console.log(`  ➜  Network: ${chalk.cyan(networkUrl)}`)
   console.log(`  ➜  press ${chalk.bold('CTRL+C')} to stop the server\n`)
