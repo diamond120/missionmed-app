c7900c699231683b4c72b6635166b57713962374
diamond120 <diamond.at312@gmail.com>
2024-11-22 11:27:27 +0530

    Revert iframe calendar functionality deprecated flag

--- src/components/layout/calendarLayout.tsx
+++ src/components/layout/calendarLayout.tsx
@@ -1,71 +1,66 @@
-import './calendarLayout.less'
-import { FC, useState, useEffect } from 'react'
-import { Tabs } from 'antd'
-import TutorCalendarTeaching from '../../pages/tutor-calendar-teaching'
-import TutorCalendarMock from '../../pages/tutor-calendar-mock'
-import { useParams } from 'react-router-dom'
-import CommonService from '../../api/services/Common'
-
-/**
- * This functionality move to `missionmed/ucat-exam-interface` repo
- * @deprecated - not in use - 2024-10-09
- */
+import "./calendarLayout.less"
+import { FC, useState, useMemo, useEffect} from "react"
+import { Tabs } from "antd"
+import TutorCalendarTeaching from "../../pages/tutor-calendar-teaching"
+import TutorCalendarMock from "../../pages/tutor-calendar-mock"
+import { useParams } from 'react-router-dom';
+import CommonService from "../../api/services/Common";
 export const calendarLayout: FC = () => {
-  const { TabPane } = Tabs
-  const [slots, setSlots] = useState([])
-  const [spin, setSpin] = useState(false)
-  const { ID } = useParams()
-  const calTutorId = parseInt(ID)
+  const { TabPane } = Tabs;
+  const [slots, setSlots] = useState([]);
+  const [spin, setSpin] = useState(false);
+  const { ID } = useParams();
+  const calTutorId = parseInt(ID); 
   useEffect(() => {
     const fetchData = async () => {
-      setSpin(true)
+      setSpin(true);
       try {
         const data = {
-          tutorId: calTutorId
-        }
-        const response = await CommonService.postAPI('/tutors-session-check', data)
+          tutorId: calTutorId,
+        };
+        const response = await CommonService.postAPI("/tutors-session-check", data);
         if (response.data.success) {
-          const slotList = response.data.data ?? []
-          setSlots(slotList)
+          const slotList = response.data.data ?? [];
+          setSlots(slotList);
         } else {
-          throw new Error(response.data.message)
+          throw new Error(response.data.message);
         }
       } catch (error) {
-        message.error(error.message)
+        message.error(error.message);
       } finally {
-        setSpin(false)
+        setSpin(false);
       }
-    }
+    };
 
-    fetchData()
-  }, [calTutorId])
-  return (
-    <div className={'book-time-cal-wrap  without-calendar-login'}>
-      <Tabs>
-        {slots.ucat_tutoring === 'true' && (
-          <TabPane tab={'UCAT Teaching Session'} key={'ucat'}>
-            <div className={'working-cal-wrap'}>
-              <TutorCalendarTeaching />
-            </div>
-          </TabPane>
-        )}
-        {slots.interview_tutoring === 'true' && (
-          <TabPane tab={'Interview Teaching Session'} key={'teaching'}>
-            <div className={'working-cal-wrap'}>
-              <TutorCalendarTeaching />
-            </div>
-          </TabPane>
-        )}
-        {slots.mock_interview === 'true' && (
-          <TabPane tab={'Mock Interview'} key={'mock'}>
-            <div className={'working-cal-wrap'}>
-              <TutorCalendarMock />
-            </div>
-          </TabPane>
-        )}
-      </Tabs>
+    fetchData();
+  }, [calTutorId]);
+return(
+<div className={"book-time-cal-wrap  without-calendar-login"}>
+  <Tabs >
+  {slots.ucat_tutoring === 'true' && (
+    <TabPane tab={"UCAT Teaching Session"} key={"ucat"}>
+    <div className={"working-cal-wrap"}>
+      <TutorCalendarTeaching />
+      </div>
+    </TabPane>
+     )}
+     {slots.interview_tutoring === 'true' && (
+    <TabPane tab={"Interview Teaching Session"} key={"teaching"}>
+    <div className={"working-cal-wrap"}>
+      <TutorCalendarTeaching />
     </div>
-  )
-}
+    </TabPane>
+      )}
+    {slots.mock_interview === 'true' && (
+    <TabPane tab={"Mock Interview"} key={"mock"}>
+    <div className={"working-cal-wrap"}>
+      <TutorCalendarMock />
+    </div>
+    </TabPane>
+   )}
+  </Tabs>
+</div>
+)
+};
 
-export default calendarLayout
+export default calendarLayout;
