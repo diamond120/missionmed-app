441fd7aaf53595d6c9712358714234689352b035
diamond120 <diamond.at312@gmail.com>
2024-11-22 14:07:35 +0530

    disable google copy to dist from server.mjs file

--- server.mjs
+++ server.mjs
@@ -10,7 +10,7 @@
 
 // app.listen(8080)
 
-import fs from 'fs'
+// import fs from 'fs'
 import express from 'express'
 import path from 'path'
 import os from 'os' // To get the network IP address
@@ -21,20 +21,20 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url) // get the resolved path to the file
 const __dirname = path.dirname(__filename) // get the name of the directory
 
-try {
-  const sourcePath = path.join(__dirname, 'google5f2b2bf91c3f04a8.html')
-  const destPath = path.join(__dirname, 'dist', 'google5f2b2bf91c3f04a8.html')
+// try {
+//   const sourcePath = path.join(__dirname, 'google5f2b2bf91c3f04a8.html')
+//   const destPath = path.join(__dirname, 'dist', 'google5f2b2bf91c3f04a8.html')
 
-  fs.copyFile(sourcePath, destPath, (err) => {
-    if (err) {
-      console.error('Error copying file:', err)
-    } else {
-      console.log('custom.html copied successfully.')
-    }
-  })
-} catch (e) {
-  console.error(e.message)
-}
+//   fs.copyFile(sourcePath, destPath, (err) => {
+//     if (err) {
+//       console.error('Error copying file:', err)
+//     } else {
+//       console.log('custom.html copied successfully.')
+//     }
+//   })
+// } catch (e) {
+//   console.error(e.message)
+// }
 
 // Serve static files from the "dist" directory
 app.use(express.static(path.join(__dirname, 'dist')))
